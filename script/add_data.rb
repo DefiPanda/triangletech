6a75215bd58c0633b4a9eb8f20ee064b8b4a02db
jw2013 <zhew@live.unc.edu>
2013-07-02 20:36:16 -0400

    add blacklist to uninteresting topic

--- script/add_data.rb
+++ script/add_data.rb
@@ -1,18 +1,19 @@
 def getParsedJSON(url, https)
-	uri = URI.parse(url)
-	http = Net::HTTP.new(uri.host, uri.port)
-	request = Net::HTTP::Get.new(uri.request_uri)
-	request.initialize_http_header({"Accept-Charset" => "utf-8"})
-	if https == 0
-	  response = http.request(request)
-	else
-	  response = Net::HTTP.start(uri.host, use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
+  uri = URI.parse(url)
+  http = Net::HTTP.new(uri.host, uri.port)
+  request = Net::HTTP::Get.new(uri.request_uri)
+  request.initialize_http_header({"Accept-Charset" => "utf-8"})
+  if https == 0
+    response = http.request(request)
+  else
+    response = Net::HTTP.start(uri.host, use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
         http.get uri.request_uri, 'User-Agent' => 'MyLib v1.2'
       end
     end
-	result = JSON.parse(response.body)
+  result = JSON.parse(response.body)
 end
 
+blacklist = ["haircut", "jazz", "hip hop"]
 # this is for querying all tech events within 50 miles of Durham,NC on Meetup
 url = "http://api.meetup.com/2/open_events?status=upcoming&radius=50.0&category=34&limited_events=False&desc=False&offset=0&format=json&zip=27701&page=500&key=5b743fa5256266b815b22f64775a"
 result = getParsedJSON(url, 0)
@@ -22,13 +23,13 @@ result["results"].each do |event|
   organizer = event["group"]["name"]
   content = event["description"]
   link = event["event_url"]
-  if Event.pair_exists?(date, title, organizer) == false
+  if Event.pair_exists?(date, title, organizer) == false and blacklist.any? { |s| title.downcase.include?(s) } == false
     Event.create!(:content => content, :date=> date, :title=> title, :organizer=> organizer, :link=> link)
   end
 end
 
 # this is for querying some tech events within 50 miles of Durham,NC on Eventbrite
-keywords = ["ruby", "javascript", "start-up", "startup", "python", "php", "web", "data", "entrepreneur", "java", "C", "hack", "social%20media", "online%20marketing", "user%20design", "ui", "ui/ux", "venture%20capital", "fundraising"]
+keywords = ["ruby", "javascript", "start-up", "startup", "python", "php", "web", "data", "entrepreneur", "java", "C", "hack", "social+media", "online+marketing", "user+design", "ui", "ui%2Fux", "venture+capital"]
 base_url = "https://www.eventbrite.com/json/event_search?app_key=WSNTTAWEAGBXTF6DEZ&postal_code=27701&within=50&keywords="
 keywords_string = ""
 keywords.each { |keyword| keywords_string << keyword << "%20OR%20" }
@@ -43,7 +44,7 @@ result.each do |event_wrapper|
   organizer = event["organizer"]["name"]
   content = event["description"]
   link = event["url"]
-  if Event.pair_exists?(date, title, organizer) == false
+  if Event.pair_exists?(date, title, organizer) == false and blacklist.any? { |s| title.downcase.include?(s) } == false
     Event.create!(:content => content, :date=> date, :title=> title, :organizer=> organizer, :link=> link)
   end
 end
