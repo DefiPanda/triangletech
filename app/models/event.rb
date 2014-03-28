4df5c77b73032d60baea81d89b649e4f647769e8
jw2013 <zhew@live.unc.edu>
2014-03-28 12:23:32 -0400

    don 't force type and id

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   attr_accessible :content, :date, :title, :organizer, :typesite, :typeid, :link
 
-  validates :content, :date, :title, :organizer, :typesite, :typeid, :presence => true
+  validates :content, :date, :title, :organizer, :presence => true
   validates :title, :length => {:maximum => 100}
 
   def start_time
